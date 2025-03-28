9e73710c335b3b87164007989301c2867f33b844
Mykyta <kosparco@gmail.com>
2025-03-28 13:33:58 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-def text_file_filterer():
+import os
+
+def get_input_data():
     input_file = input("Введіть назву файлу: ").strip()
     keyword = input("Введіть ключове слово: ").strip()
-    output_file = "filtered.txt"
+    text_file_filterer(input_file, keyword)
+
+def text_file_filterer(input_file, keyword):
+    output_file_path = os.path.join(os.path.dirname(input_file), "filtered.txt")
 
     try:
-        with open(input_file, "r", encoding="utf-8") as infile, open(output_file, "w", encoding="utf-8") as outfile:
+        with open(input_file, "r", encoding="utf-8") as infile, open(output_file_path, "w", encoding="utf-8") as outfile:
             for line in infile:
-                if keyword in line:
+                if keyword.lower() in line.lower():
                     outfile.write(line)
 
-        print(f"Фільтровані рядки збережено у {output_file}")
+        print(f"Фільтровані рядки збережено у {output_file_path}")
 
     except FileNotFoundError:
         print("Файл не знайдено. Перевірте правильність назви.")
 
-text_file_filterer()
+if __name__ == "__main__":
+    get_input_data()
